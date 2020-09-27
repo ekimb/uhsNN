37947599a24d3aa659170552325534a8c62bcf1d
Barış Ekim <ekimb@mit.edu>
2020-09-27 22:01:44 +0300

    fix size print

--- predict.cc
+++ predict.cc
@@ -57,6 +57,6 @@ int main(int argc, char* argv[]) {
     elapsed = (finish.tv_sec - start.tv_sec);
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
     cout << elapsed << " seconds." << endl; 
-    cout << "Hitting set size: " << hittingSize << endl << endl;
+    cout << "Hitting set size: " << hittingSize + decyclingSize << endl << endl;
     return 0;
 }
