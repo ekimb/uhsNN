64707dd41af0a172ad47163f5df7e8d8575db283
Barış Ekim <ekimb@mit.edu>
2020-09-27 20:58:07 +0300

    switch to pdocks

--- predict.cc
+++ predict.cc
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
         r.pred = std::stof(s);
         v.push_back(r);
     }
-    int hittingSize = docks.HittingRandomParallel(L, hittingPath, threshold, threads, v);
+    int hittingSize = docks.HittingML(L, hittingPath, threshold, threads, v);
     clock_gettime(CLOCK_MONOTONIC, &finish);
     elapsed = (finish.tv_sec - start.tv_sec);
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
